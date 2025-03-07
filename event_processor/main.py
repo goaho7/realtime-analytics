167fb4c07364b0db3d2c710176db61c546e139c8
goaho7 <goaho7@gmail.com>
2025-03-07 13:24:54 +0400

    fix 1

--- event_processor/main.py
+++ event_processor/main.py
@@ -1,18 +1,45 @@
 import asyncio
+import logging
+import sys
+
+from confluent_kafka import KafkaException
 
 from src.service.kafka_consumer import KafkaConsumerBase
-from src.repositories.event import EventRepository
 
-if __name__ == '__main__':
-    kafka_config = {
-        'bootstrap.servers': 'kafka:9093',
-        'group.id': 'event-consumer-group',
-        'auto.offset.reset': 'latest'
-    }
 
-    consumer = KafkaConsumerBase(config=kafka_config, topics=['sensor-events'])
+MAX_RETRIES = 3
+RETRY_DELAY = 5
+KAFKA_CONFIG = {
+    "bootstrap.servers": "broker:29092",
+    "group.id": "event-consumer-group",
+    "auto.offset.reset": "latest",
+}
 
-    consumer.create_consumer()
-    consumer.subscribe_to_topics()
 
-    asyncio.run(consumer.consume_messages()) 
+async def main():
+    consumer = KafkaConsumerBase(config=KAFKA_CONFIG, topics=["sensor-events"])
+    retry_delay = RETRY_DELAY
+
+    for attempt in range(MAX_RETRIES):
+        try:
+            consumer.subscribe_to_topics()
+            break
+        except KafkaException:
+            logging.error(
+                f"Повторная попытка подписки через {retry_delay} секунд (попытка {attempt + 1}/{MAX_RETRIES})"
+            )
+            await asyncio.sleep(retry_delay)
+            retry_delay *= 2
+    else:
+        logging.error("Превышено количество попыток подписки. Завершение.")
+        sys.exit(1)
+
+    await consumer.run_consumer()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logging.error(f"Критическая ошибка: {e}")
+        sys.exit(1)
