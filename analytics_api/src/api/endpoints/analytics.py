478e87a5e87d03b7535b8155d9c97dc44b075000
goaho7 <goaho7@gmail.com>
2025-04-09 23:28:29 +0400

    fix celery

--- analytics_api/src/api/endpoints/analytics.py
+++ analytics_api/src/api/endpoints/analytics.py
@@ -1,16 +1,11 @@
-from typing import Annotated, List, Optional
-from datetime import datetime
+from typing import List, Optional
 
 from fastapi import APIRouter, Query, Depends
 
 from src.schemas.stats import SensorStat
 from src.repositories.analytics import AnalyticsRepository
-from src.service.s3_service import S3Service
 from src.service.s3_client import S3Client
-from src.api.depensens import get_s3_service, get_s3_client
-
-# from src.celery_core.tasks import upload_daily_file
-from src.celery_core.tasks import add
+from src.api.depensens import get_s3_client
 
 
 router = APIRouter()
@@ -38,16 +33,3 @@ async def get_list_files(
 @router.get("/get-file")
 async def get_file(file_name: str, s3_client: S3Client = Depends(get_s3_client)):
     return await s3_client.download_file(file_name)
-
-
-# @router.get("/test")
-# async def test(s3_service: Annotated[S3Service, Depends(get_s3_service)]):
-#     return await s3_service.upload_file()
-
-
-@router.get("/trigger-task")
-async def trigger_task():
-    # task = upload_daily_file.delay()
-    # return {"message": "Task triggered", "task_id": task.id}
-    task = add.delay(2, 3)
-    return {"task_id": task.id}
